8fe5db9fc5f34ea32e6c30fdc0329da2f9504973
abeilleabejorro <abeille.abejorro@gmail.com>
2015-06-02 09:52:00 -0400

    started rails app

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,4 @@
+require "yaml"
 require File.expand_path('../boot', __FILE__)
 
 require "rails"
@@ -10,6 +11,7 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
