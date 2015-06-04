ccd885e1eba4d658b77ac3ef7aad89fe0b233c7a
abeilleabejorro <abeille.abejorro@gmail.com>
2015-06-04 12:01:36 -0400

    added logic and sessions and url redirect to email

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -28,15 +28,15 @@ before_action :require_login, only: [:show, :edit, :update]
   end
 
   def update
-    binding.pry
+    # binding.pry
     @trip = Trip.find(params["id"])
     if User.find_by(email: params["trip"]["members"])
       @member = User.find_by(email: params["trip"]["members"])
+      MyMailer.add_existing_member(@member, @trip).deliver_now
       session["member"] = @member
     else #user needs to sign up
       @member = User.new(email: params["trip"]["members"])
     end
-    MyMailer.add_member(@member, @trip).deliver_now
 
     render nothing: true
   end
@@ -51,9 +51,11 @@ private
 
     def require_login
     unless signed_in?
+      #if you're not signed in, check if user is a member (in database)
+      # binding.pry
       flash[:error] = "You must be logged in to access this section"
       session["path"]=request.path
-      binding.pry
+      # binding.pry
       redirect_to new_user_registration_path #If the user doesn't exist
       #else take them to login and redirect to "do you want to join group?"
     end
@@ -63,3 +65,5 @@ private
     params.require(:trip).permit(:name, :description, :origin, :destination)
   end
 end
+
+
