444414102aa5283a30e4611909427f00f39436ea
najjar.dana@gmail.com <najjar.dana@gmail.com>
2015-06-04 15:42:11 -0400

    view trip

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,25 +1,17 @@
 class RegistrationsController < Devise::RegistrationsController
   after_action
 
-  def create
-  #log in
-    if session["path"]  
-      #associate them to the group
-      redirect_to show
-    end 
-  end 
 
   def new
     @user = User.new
   end
 
   def create
-    #log in
+
     @user = User.create(sign_up_params)
 
     if session["path"]
-      #associate them to the group
-      @trip = Trip.find(session["path"].split("/")[2].to_i)
+r      @trip = Trip.find(session["path"].split("/")[2].to_i)
       @trip.members << @user
       @user.trips << @trip
       redirect_to session["path"]
