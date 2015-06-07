cb21114a45ecc95fc91860abf79d480aa0662aa6
lucy-orbach <lucy.orbach@gmail.com>
2015-06-07 16:59:09 -0400

    front-end integration

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,41 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+ 
+ 
+ //=require plugins/modernizr-latest.js
+ //=require plugins/picturefill.js
+ //=require plugins/smoothScroll.js
+ //=require plugins/wow.js
+ //=require plugins/classie.js
+ //=require plugins/jquery.nav.js
+ //=require plugins/jquery.stellar.js
+ //=require plugins/jquery.themepunch.tools.js
+ //=require plugins/jquery.themepunch.revolution.js
+ //=require plugins/owl.carousel.js
+ //=require plugins/jquery.knob.js
+ //=require plugins/form.js
+ //=require plugins/dialogFx.js
+ //=require plugins/jquery.cubeportfolio.js
+ //=require plugins/awesome-work.js
+ //=require plugins/jquery.modalEffects.js
+ //=require plugins/cssParser.js
+ 
+ //=require plugins/snazzy-maps.js
+//=require plugins.js
+ //=require custom.js
+ //=require style-switcher.js
+
+
+
 
 //= require_tree .
+
+
+
+
+
