b5007486f9ef2c35724d7cbd39709792c2061cc8
lucy-orbach <lucy.orbach@gmail.com>
2015-06-08 18:28:56 -0400

    minor changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,27 +18,27 @@
 //= require trips
 
 
- //=require ../../../vendor/assets/javascripts/plugins/modernizr-latest.js
- //=require ../../../vendor/assets/javascripts/plugins/picturefill.js
- //=require ../../../vendor/assets/javascripts/plugins/smoothScroll.js
- //=require ../../../vendor/assets/javascripts/plugins/wow.js
- //=require ../../../vendor/assets/javascripts/plugins/classie.js
- //=require ../../../vendor/assets/javascripts/plugins/jquery.nav.js
- //=require ../../../vendor/assets/javascripts/plugins/jquery.stellar.js
- //=require ../../../vendor/assets/javascripts/plugins/jquery.themepunch.tools.js
- //=require ../../../vendor/assets/javascripts/plugins/jquery.themepunch.revolution.js
- //=require ../../../vendor/assets/javascripts/plugins/owl.carousel.js
- //=require ../../../vendor/assets/javascripts/plugins/jquery.knob.js
- //=require ../../../vendor/assets/javascripts/plugins/form.js
- //=require ../../../vendor/assets/javascripts/plugins/dialogFx.js
- //=require ../../../vendor/assets/javascripts/plugins/jquery.cubeportfolio.js
- //=require ../../../vendor/assets/javascripts/plugins/awesome-work.js
- //=require ../../../vendor/assets/javascripts/plugins/jquery.modalEffects.js
- //=require ../../../vendor/assets/javascripts/plugins/cssParser.js
+ //=require plugins/modernizr-latest.js
+ //=require plugins/picturefill.js
+ //=require plugins/smoothScroll.js
+ //=require plugins/wow.js
+ //=require plugins/classie.js
+ //=require plugins/jquery.nav.js
+ //=require plugins/jquery.stellar.js
+ //=require plugins/jquery.themepunch.tools.js
+ //=require plugins/jquery.themepunch.revolution.js
+ //=require plugins/owl.carousel.js
+ //=require plugins/jquery.knob.js
+ //=require plugins/form.js
+ //=require plugins/dialogFx.js
+ //=require plugins/jquery.cubeportfolio.js
+ //=require plugins/awesome-work.js
+ //=require plugins/jquery.modalEffects.js
+ //=require plugins/cssParser.js
 
- //=require ../../../vendor/assets/javascripts/plugins.js
- //=require ../../../vendor/assets/javascripts/custom.js
- //=require ../../../vendor/assets/javascripts/style-switcher.js
+ //=require plugins.js
+ //=require custom.js
+ //=require style-switcher.js
 
 
 
