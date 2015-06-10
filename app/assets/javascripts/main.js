a06659e1ca0fb39a01e3057c91774b66e05cd6e2
lucy-orbach <lucy.orbach@gmail.com>
2015-06-10 14:27:59 -0400

    autocomplete is frustating

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -12,6 +12,10 @@ $(document).ready(function(){
     Plugins.knobs(".lbKnob", "#26C6DA");
     Plugins.modalEffects();
 
+    $('.places').click(function(){
+        initAutoComplete();
+    });
+
     //Custom JS Init
     Custom.init();
     Custom.captchaFix();
@@ -19,10 +23,42 @@ $(document).ready(function(){
     Custom.mobileMenuAnimation();
 
     
-});
+});//document ready
+
+//Google places autocomplete
+function initAutoComplete(){
+    var input = $('.places');
+    var options = {
+        types: ['(cities)'],
+        componentRestrictions: {country: "us"}
+    };
+    
+
+    var autoComplete = new google.maps.places.Autocomplete( input, options ); 
+
+    debugger;
+    
+       
+}//autocomplete
+
+// google.maps.event.addDomListener(window, 'load', initAutoComplete);
+
+// google.maps.event.addListener(autoComplete, 'place_changed', function() {
+//   var place = autocomplete.getPlace();
+   
+// });
+ 
 
 
 
+
+
+
+
+
+
+    
+    
 
 
 
