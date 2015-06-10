29b2886959fd9256d8f8306e25457e1c481850f1
lucy-orbach <lucy.orbach@gmail.com>
2015-06-10 18:03:17 -0400

    cleaned htmls

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -12,23 +12,8 @@ $(document).ready(function(){
     Plugins.knobs(".lbKnob", "#26C6DA");
     Plugins.modalEffects();
 
-    $('.places').click(function(){
-        var input = $('.places');
-        var options = {
-            types: ['(cities)']
-            //, componentRestrictions: {country: "us"}
-        };
-    
-        var autoComplete = new google.maps.places.Autocomplete( input, options ); 
-        var searchBox = new google.maps.places.SearchBox( input, options );
-    });//click function
-    
-        google.maps.event.addListener(autoComplete, 'place_changed', function() {
-            var place = autocomplete.getPlace();
-        });
- 
 
-    
+ 
     //Custom JS Init
     Custom.init();
     //Custom.captchaFix();
@@ -37,19 +22,7 @@ $(document).ready(function(){
     
 });//document ready
 
-//Google places autocomplete
-function autoComplete(){
-    
 
-    debugger;
-
-
-
-       
-}//autocomplete
-
-
-// google.maps.event.addDomListener(window, 'load', initAutoComplete);
 
 
 
--- app/assets/javascripts/trips.js
+++ app/assets/javascripts/trips.js
@@ -1,4 +1,44 @@
 $(document).ready(function(){
+  inviteMore();
+  getMaps();
+});
+
+
+function getMaps(){
+   var directionsDisplay;
+    var directionsService = new google.maps.DirectionsService();
+    var map;
+
+    function initialize() {
+      directionsDisplay = new google.maps.DirectionsRenderer();
+      var thehome = new google.maps.LatLng(40.4764, -106.8267);
+      var mapOptions = {
+        zoom:7,
+        center: thehome
+      };
+      map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+      directionsDisplay.setMap(map);
+      calcRoute();
+    }
+
+    function calcRoute() {
+      var start = document.getElementById('start').value;
+      var end = document.getElementById('end').value;
+      var request = {
+          origin:start,
+          destination:end,
+          travelMode: google.maps.TravelMode.DRIVING
+      };
+      directionsService.route(request, function(response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+          directionsDisplay.setDirections(response);
+        }
+      });
+    }
+   google.maps.event.addDomListener(window, 'load', initialize);
+}
+
+function inviteMore(){
   $("#invite").click(function(e){
     if ($("#email1").val()=="" && $("#email2").val()=="" && $("#email2").val()=="" ){
       e.preventDefault();
@@ -12,7 +52,7 @@ $(document).ready(function(){
    $(".email-fld:last-child").append('<li class="email-fld"><span class="pic-thumb"><i class="fa fa-user"></i></span><input type="email" name="email[]" placeholder="friend@friend.com" id="email4"></li>');
                                     
   });
-});
+}
 
 
 
