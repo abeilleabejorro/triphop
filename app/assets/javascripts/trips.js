4f94fd06a90c426675918415e7cbb600ca126052
lucy-orbach <lucy.orbach@gmail.com>
2015-06-12 09:54:31 -0400

    minor changes

--- app/assets/javascripts/trips.js
+++ app/assets/javascripts/trips.js
@@ -50,6 +50,7 @@ if ($("#trip-edit-page").length>0){
 function inviteMore(){
   if ($("#trip-edit-page").length>0){
   $("#invite").click(function(e){
+    
     if ($("#email1").val()=="" && $("#email2").val()=="" && $("#email2").val()=="" ){
       e.preventDefault();
       alert("please enter at least one email address!");
@@ -58,8 +59,11 @@ function inviteMore(){
   });
   $(".add-email").click(function(e){
     //debugger;
+
   e.preventDefault();
-   $(".email-fld:last-child").append('<li class="email-fld"><span class="pic-thumb"><i class="fa fa-user"></i></span><input type="email" name="email[]" placeholder="friend@friend.com" id="email4"></li>');
+
+  var lastChild = $(".email-fld");
+  lastChild.parent().append('<li class="email-fld"><span class="pic-thumb"><i class="fa fa-user"></i></span><input type="email" name="email[]" placeholder="friend@friend.com" id="email4"></li>');
 
     });
   }
